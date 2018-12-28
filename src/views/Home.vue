541372c23aaa8dacc02d5b8e53594a704a7f677f
todorpanev <toshopanev@gmail.com>
2018-12-28 12:44:42 +0200

    omitting nav component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,13 @@
 <template>
 	<div>
-		<HomeNav></HomeNav>
+		<nav>
+			<a href="#" v-scroll-to="'#hello'">Hello</a>
+			<a href="#" v-scroll-to="'#about'">About</a>
+			<a href="#" v-scroll-to="'#works'">Work</a>
+			<a href="#" v-scroll-to="'#contact'">Contact</a>
+		</nav>
 		<Hello></Hello>
-		<About v-on:inAbout="omg"></About>
+		<About v-on:isActive="omg"></About>
 		<Works></Works>
 		<Contact></Contact>
 	</div>
@@ -10,7 +15,6 @@
 
 <script>
 // @ is an alias to /src
-import HomeNav from '@/components/HomeNav.vue'
 import Hello from '@/components/Hello.vue'
 import About from '@/components/About.vue'
 import Works from '@/components/Works.vue'
@@ -19,7 +23,6 @@ import Contact from '@/components/Contact.vue'
 export default {
   name: 'home',
   components: {
-    HomeNav,
     Hello,
     About,
     Works,
@@ -32,3 +35,22 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+@import '@/scss/utils.scss';
+
+nav {
+	height: $nav-height;
+	background: #fff;
+	position: fixed;
+	top: 0;
+	width: 100%;
+}
+
+a {
+	margin: 0 10px;
+	display: inline-block;
+	font-size: 20px;
+}
+
+</style>
