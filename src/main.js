2e71bd63a4b331e86eb1dd41c522cc3c40365df3
todorpanev <toshopanev@gmail.com>
2019-01-13 00:07:30 +0200

    adding google analytics

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueScrollTo from 'vue-scrollto'
+import VueAnalytics from 'vue-analytics'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
 Vue.use(VueScrollTo)
+
+Vue.use(VueAnalytics, {
+  id: 'UA-132321652-1',
+  router
+})
 
 new Vue({
   router,
