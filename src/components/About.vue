dbcedc4f0618ca7342cbd336737644ae55dfbb51
todorpanev <toshopanev@gmail.com>
2019-01-22 20:24:50 +0200

    about copy update

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,13 +2,11 @@
 	<div id="about" ref="about" class="grid-container">
 		<h1 class="about-heading section-heading">About me</h1>
 		<img src="@/assets/aboutGraph.svg" alt="timeline graph">
-		<p>I've spent the last decade of my career exploring visual communication, learning about the intracacies that technology has to offer and delving into what user experience is and applying it in the real world.
-		<br>
-		<br>
-		I believe that technology is an integral part of any designer's toolkit. Being able to code &amp; keeping up to date with front-end has vastly expanded my outlook towards the products that I design and build.
-		<br>
-		<br>
-		My current focus is doing product design, where I strive to optimize complexity and prevent noise in digital products.</p>
+		<p>
+			I've spent most of my career helping build digital products, learning about the intracacies that  the tech world has to offer and exploring what user experience is and applying it to the real world.
+			<br><br>
+			My current interests lie in understanding complex problems holistically and applying solutions that have a positive impact throughout the entire system or product. 
+		</p>
 	</div>
 </template>
 
