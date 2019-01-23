b1efd964b4ce52695b1fdd7ce61334a01eab9620
todorpanev <toshopanev@gmail.com>
2019-01-23 17:27:44 +0200

    about section read more

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,13 +2,21 @@
 	<div id="about" ref="about" class="grid-container">
 		<h1 class="about-heading section-heading">About me</h1>
 		<img src="@/assets/aboutGraph.svg" alt="timeline graph">
-		<p>I've spent the last decade of my career exploring visual communication, learning about the intracacies that technology has to offer and delving into what user experience is and applying it in the real world.
-		<br>
-		<br>
-		I believe that technology is an integral part of any designer's toolkit. Being able to code &amp; keeping up to date with front-end has vastly expanded my outlook towards the products that I design and build.
-		<br>
-		<br>
-		My current focus is doing product design, where I strive to optimize complexity and prevent noise in digital products.</p>
+		<div class="initial-info info-block">
+			<p v-if="!personWantsToKnowMore">
+				I've spent most of my career helping build digital products, learning about the intracacies that the web technology has to offer and exploring what user experience is and applying it to the real world.
+				<br><br>
+				My current interests lie in understanding complex problems holistically and applying solutions that have a positive impact throughout the entire system or product. 
+			</p>
+			<p v-if="personWantsToKnowMore">
+				I've spent the last 10 years doing[?] visual communication, learning about the intracacies that technology has to offer and exploring what user experience is and applying it to the real world.
+				<br><br>
+				[too harsh of a context-switch] I believe that technology is an integral part of any designer's toolkit. Being able to code & keeping up to date with front-end has vastly expanded my outlook towards the products that I design and build.
+				<br><br>
+				My current focus and interests lie in understanding complex problems holistically and applying solutions  is doing user experience design, where I strive to optimize complexity and prevent noise in digital products.
+			</p>
+			<a class="button-slim" @click="toggleMoreInfo()">{{ personWantsToKnowMore ? 'READ LESS' : 'READ MORE' }}</a>
+		</div>
 	</div>
 </template>
 
@@ -18,7 +26,8 @@ export default {
 	data: function() {
 		return {
 			aboutAreaStart: 0,
-			aboutAreaEnd: 0		
+			aboutAreaEnd: 0,
+			personWantsToKnowMore: false
 		}
 	},
 	methods: {
@@ -28,6 +37,14 @@ export default {
 			this.$emit('aboutAreaStartCalculated', this.aboutAreaStart)
 			this.$emit('aboutAreaEndCalculated', this.aboutAreaEnd)
 		},
+		toggleMoreInfo() {
+			if (this.personWantsToKnowMore === false) {
+				this.personWantsToKnowMore = true
+			}
+			else {
+				this.personWantsToKnowMore = false
+			}
+		}
 	},
 	mounted() {
 		this.getAboutArea();
@@ -57,10 +74,17 @@ export default {
     }
 }
 
-p {
+.info-block {
 	grid-column: 6 / 11;
 	text-align: left;
 	align-self: center;
+}
+
+.button-slim {
+	grid-column: 6 / 8;
+	text-align: left;
+	align-self: top;
+
 }
 
 img {
