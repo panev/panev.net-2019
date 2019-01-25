d95a64b6264ba02a7d147c1fb71c3ccfa398287c
todorpanev <toshopanev@gmail.com>
2019-01-25 16:40:06 +0200

    about section thumbnail animation, yeah

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,7 +3,29 @@
 		<h1 class="about-heading section-heading">
 			About me
 			<div class="heading-ani">
-				<img src="@/assets/homeAbout.svg" alt="section thumbnail" style="height: 102px;">	
+				<svg width="91px" height="132px" viewBox="0 0 91 132" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+				    <g id="Visual-playground" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+				        <g id="Home-Full-HD-Copy-6" transform="translate(-831.000000, -768.000000)">
+				            <g id="Group-4" transform="translate(831.000000, 768.000000)">
+				            	<g id="Rocket">
+					                <ellipse id="Oval-7-Copy-4" fill="#62DCDC" cx="24.5" cy="84.5" rx="7" ry="12.5"></ellipse>
+					                <ellipse id="Oval-7-Copy-5" fill="#62DCDC" cx="48.5" cy="84.5" rx="7" ry="12.5"></ellipse>
+					                <ellipse id="Oval-7" fill="#F0F0F0" cx="35" cy="69.5" rx="12.5" ry="39.5"></ellipse>
+					                <circle id="Oval-8" fill="#C1C1C1" cx="35" cy="54.5" r="4.5"></circle>
+					                <ellipse id="Oval-7-Copy" fill="#C1C1C1" cx="35" cy="97.5" rx="2.5" ry="15.5"></ellipse>
+					                <g id="Beams" transform="translate(0, 115)">>
+						                <rect id="Beam-1" fill="#72E0E0" x="28" y="-5" width="3" height="13" rx="1.5"></rect>
+						                <rect id="Beam-2" fill="#72E0E0" x="39" y="0" width="3" height="13" rx="1.5"></rect>
+						                <rect id="Beam-3" fill="#72E0E0" x="33.5" y="5" width="3" height="14" rx="1.5"></rect>
+					            	</g>
+				            	</g>
+				                <circle id="Oval-1" fill="#D8D8D8" fill-rule="nonzero" cx="3" cy="26" r="3"></circle>
+				                <circle id="Oval-2" fill="#F0F0F0" fill-rule="nonzero" cx="85" cy="0" r="6"></circle>
+				                <polygon id="Triangle-1" fill="#E2E2E2" fill-rule="nonzero" points="33 1.73471144 44.8266177 3.8191672e-14 40.0474205 11"></polygon>
+				            </g>
+				        </g>
+				    </g>
+				</svg>
 			</div>
 		</h1>
 		<img src="@/assets/aboutGraph.svg" alt="timeline graph" class="timeline-graph">
@@ -183,5 +205,125 @@ export default {
 	grid-column: 2 / 5;
 	height: 760px;
 }
+
+
+
+#Oval-1 {
+	animation: pass 10s infinite;
+
+}
+
+#Oval-2 {
+	animation: pass 5s infinite;
+}
+
+#Oval-1 {
+	animation-delay: 0.75s;
+} 
+
+@keyframes pass {
+	0% {
+		opacity: 0;
+		transform: translateY(0);
+	}
+
+	50% {
+		opacity: 1;
+	}
+
+	100% {
+		opacity: 0;
+		transform: translateY(150px);
+	}
+}
+
+#Triangle-1 {
+	animation: pass2 4s infinite;
+	transform-origin: center center;
+}
+
+@keyframes pass2 {
+	0% {
+		opacity: 0;
+		transform: translate(0,0) rotate(0deg);
+	}
+
+	50% {
+		opacity: 1;
+	}
+
+	100% {
+		opacity: 0;
+		transform: translate(20px, 50px) rotate(60deg);
+	}
+}
+
+#Rocket {
+	animation: rocket-wiggle 4s infinite;
+}
+
+@keyframes rocket-wiggle {
+	0% {
+		transform: translate(0px, 0px);
+	}
+
+	55% {
+		transform: translate(5px, -6px);
+	}
+
+
+	100% {
+		transform: translate(0px, 0px);
+	}
+
+}
+
+#Beam-1,
+#Beam-2,
+#Beam-3 {
+	animation: beam 1s infinite;
+	transform-origin: top;
+}
+
+#Beam-1 {
+	animation-delay: 0.3s;
+}
+
+#Beam-2 {
+	animation-delay: 1.5s;
+}
+
+#Beam-3 {
+	animation-delay: 0.75s;
+}
+
+@keyframes beam {
+	0% {
+		transform: scaleY(1);
+	}
+
+	25% {
+		transform: scaleY(1.1);
+		opacity: 1;
+	}
+
+	75% {
+		transform: scaleY(0.75);
+		opacity: 0.6;
+	}
+
+	100% {
+		transform: scaleY(1);
+	}
+}
+
+svg {
+	overflow: visible;
+}
+
+
+
+
+
 
 </style>
