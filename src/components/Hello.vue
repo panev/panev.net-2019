25abb10a48ac3cd44703b43cfc573097a87942a7
todorpanev <toshopanev@gmail.com>
2019-01-28 15:12:07 +0200

    vd thumbnail animation

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -65,7 +65,7 @@
                                     <circle id="Oval" fill="#FFFFFF" cx="14.5" cy="14.5" r="8.5"></circle>
                                     <circle id="Oval" fill="#A68AFF" cx="14.5" cy="14.5" r="4.5"></circle>
                                 </g>
-                                <polygon id="Rectangle" fill="#D8D8D8" transform="translate(17.427027, 81.431179) rotate(60.000000) translate(-17.427027, -81.431179) " points="6.8585178 70.8740152 27.9758858 70.8936651 27.9955357 91.9883433 6.87816771 91.9686934"></polygon>
+                                <polygon id="p-rectangle" fill="#D8D8D8" transform="translate(17.427027, 81.431179) rotate(60.000000) translate(-17.427027, -81.431179) " points="6.8585178 70.8740152 27.9758858 70.8936651 27.9955357 91.9883433 6.87816771 91.9686934"></polygon>
                             </g>
                         </g>
                     </g>
@@ -74,9 +74,7 @@
 
                 <transition name="swap-thumb">
                 <svg v-if="activePhoto === 2" width="111px" height="111px" viewBox="0 0 111 111" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                    <title>Group 6</title>
-                    <desc>Created with Sketch.</desc>
-                    <g id="Icons-&amp;-Graph" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                    <g id="vd" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                         <g id="Group-17" transform="translate(-170.000000, -4.000000)" fill="#FFFFFF" fill-rule="nonzero">
                             <g id="Group-16">
                                 <g id="Group-15">
@@ -85,16 +83,16 @@
                             </g>
                         </g>
                         <g id="Group-6" fill-rule="nonzero">
-                            <polygon id="Rectangle-Copy-2" fill="#2B507D" transform="translate(93.000000, 86.000000) rotate(-90.000000) translate(-93.000000, -86.000000) " points="91 84 95 85.0466324 92.0466324 88"></polygon>
+                            <polygon id="vd-particle-4" fill="#2B507D" transform="translate(93.000000, 86.000000) rotate(-90.000000) translate(-93.000000, -86.000000) " points="91 84 95 85.0466324 92.0466324 88"></polygon>
                             <path d="M56,83 C67.5259426,83 80.8592759,73.852459 96,55.557377 C80.8592759,37.852459 67.5259426,29 56,29 C44.4740574,29 31.1407241,37.852459 16,55.557377 C31.1407241,73.852459 44.4740574,83 56,83 Z" id="Oval" fill="#D8D8D8"></path>
                             <path d="M56,83 C67.5259426,83 80.8592759,73.852459 96,55.557377 C80.8592759,37.852459 67.5259426,29 56,29 C44.4740574,29 31.1407241,37.852459 16,55.557377 C31.1407241,73.852459 44.4740574,83 56,83 Z" id="Oval" fill="#EFEFEF"></path>
                             <circle id="Oval" fill="#FFFFFF" cx="56" cy="56" r="16"></circle>
-                            <circle id="Oval" fill="#2B507D" cx="56" cy="56" r="14"></circle>
-                            <circle id="Oval" fill="#FFFFFF" cx="56" cy="56" r="7"></circle>
-                            <circle id="Oval-Copy-3" fill="#EFEFEF" cx="19" cy="31" r="4"></circle>
-                            <polygon id="Rectangle-Copy" fill="#E3E3E3" transform="translate(103.500000, 25.000000) rotate(-90.000000) translate(-103.500000, -25.000000) " points="95.5 17.5 111.5 21.4248717 99.6865298 32.5"></polygon>
-                            <polygon id="Rectangle" fill="#EFEFEF" transform="translate(30.648151, 91.234754) rotate(-330.000000) translate(-30.648151, -91.234754) " points="28.3518159 88.9257802 32.9225945 88.9476716 32.9444859 93.5437282 28.3737074 93.5218368"></polygon>
-                            <polygon id="Rectangle-Copy-3" fill="#EFEFEF" transform="translate(62.286343, 13.295595) rotate(-330.000000) translate(-62.286343, -13.295595) " points="57.6936729 8.67764728 66.8352301 8.72143016 66.8790129 17.9135434 57.7374558 17.8697605"></polygon>
+                            <circle id="vd-oval-1" fill="#2B507D" cx="56" cy="56" r="14"></circle>
+                            <circle id="vd-oval-2" fill="#FFFFFF" cx="56" cy="56" r="7"></circle>
+                            <circle id="vd-particle-1" fill="#EFEFEF" cx="19" cy="31" r="4"></circle>
+                            <polygon id="vd-particle-5" fill="#E3E3E3" transform="translate(103.500000, 25.000000) rotate(-90.000000) translate(-103.500000, -25.000000) " points="95.5 17.5 111.5 21.4248717 99.6865298 32.5"></polygon>
+                            <polygon id="vd-particle-2" fill="#EFEFEF" transform="translate(30.648151, 91.234754) rotate(-330.000000) translate(-30.648151, -91.234754) " points="28.3518159 88.9257802 32.9225945 88.9476716 32.9444859 93.5437282 28.3737074 93.5218368"></polygon>
+                            <polygon id="vd-particle-3" fill="#EFEFEF" transform="translate(62.286343, 13.295595) rotate(-330.000000) translate(-62.286343, -13.295595) " points="57.6936729 8.67764728 66.8352301 8.72143016 66.8790129 17.9135434 57.7374558 17.8697605"></polygon>
                         </g>
                     </g>
                 </svg>
@@ -376,16 +374,16 @@ export default {
 @keyframes sunscale {
     0% {
         transform: scale(1);
-        opacity: 0;
-    }
-
-    50% {
         opacity: 1;
     }
 
-    100% {
+    50% {
         opacity: 0;
         transform: scale(1.1);
+    }
+
+    100% {
+        opacity: 1;
     }
 }
 
@@ -411,7 +409,7 @@ export default {
 
 #p-oval-1,
 #p-oval-2 {
-    animation: scaleOvals 1.5s infinite;
+    animation: aniOvals 1.5s infinite;
     transform-origin: 50% 50%;
 }
 
@@ -420,7 +418,7 @@ export default {
 }
 
 
-@keyframes scaleOvals {
+@keyframes aniOvals {
     0% {
         opacity: 1;
     }
@@ -439,7 +437,7 @@ export default {
     transform-origin: center;
 }
 
-#Rectangle {
+#p-rectangle {
     animation: heartscale 1s infinite linear;
     transform-origin: 50% 50%;
 }
@@ -469,6 +467,87 @@ export default {
         transform: rotate(360deg);
     }
 }
+
+#vd-oval-2 {
+    animation: aniIris 1s infinite;
+    animation-delay: 1.5s;
+    transform-origin: 50% 50%;
+}
+
+#vd-oval-1 {
+    animation-delay: 0.5s;
+    animation: aniIris 2s infinite;
+    transform-origin: 50% 50%;
+}
+
+@keyframes aniIris {
+    0% {
+        transform: scale(0);
+        opacity: 0;
+    }
+    50% {
+        transform: scale(1);
+        opacity: 1;
+    }
+    100% {
+        transform: scale(0);
+        opacity: 0;
+    }
+}
+
+#vd-particle-1,
+#vd-particle-2,
+#vd-particle-3,
+#vd-particle-4,
+#vd-particle-5 {
+    animation: aniParticles 1.5s infinite;
+    transform-origin: 50% 50%;
+}
+
+#vd-particle-1 {
+    animation-delay: 0.3s;
+}
+
+#vd-particle-2 {
+    animation-delay: 0.6s;
+}
+
+#vd-particle-3 {
+    animation-delay: 0s;
+}
+
+#vd-particle-4 {
+    animation-delay: 1s;
+}
+
+#vd-particle-5 {
+    animation-delay: 0.45s;
+}
+
+
+
+@keyframes aniParticles {
+    0% {
+        transform: scale(0.5) rotate(0deg);
+        opacity: 0;
+    }
+
+    60% {
+        opacity: 1;
+    }
+
+    100% {
+        transform: scale(1.3) rotate(20deg);
+        opacity: 0;
+    }
+}
+
+
+
+
+
+
+
 
 
 svg {
